5a64f553f67bb26d8c25dfa4418636d151c90227
Arpit <arpitrai1809@gmail.com>
2025-07-27 22:16:09 +0530

    fix : frontend/dist error

--- backend/src/app.js
+++ backend/src/app.js
@@ -56,6 +56,8 @@ app.use("/api/message", messageRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  console.log("STATIC PATH:", path.join(__dirname, "../../frontend/dist"));
+
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
